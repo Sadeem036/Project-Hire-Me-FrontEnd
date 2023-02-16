fbf5dd4c259d127ad6f4d6cbcbdb629dca605780
Mujahid shairf <mujahidsharif944@gmail.com>
2023-02-16 10:33:53 +0500

    updated Frontend

--- src/components/customer-status/customer.jsx
+++ src/components/customer-status/customer.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import style from '../customer-status/customer.module.css'
+import Router, { useRouter } from 'next/router'
 export default function CustomerStatus() {
+    const router = useRouter()
     return (
         <div className={style.container}>
             <div className={style.status} >
 
-                <button className={style.button1}  >Active</button>
+                <button onClick={()=> router.push("/")} type ="submit" className={style.button1}  >Active</button>
 
                 <button className={style.button2}>Pending</button>
 
-                <button className={style.button3}>Completed</button>
+                <button onClick={()=> router.push("/complete-service")} type ="submit" className={style.button3}>Completed</button>
             </div>
             <div className={style.services}>
-                <button className={style.service}>Select Services</button>
+                <button onClick={()=> router.push("/service-category")} type ="submit" className={style.service}>Select Services</button>
+
             </div>
         </div>
     )
